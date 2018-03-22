6184fc0b8a717886ca53e364c5b1ecbe548e3186
Pingoo <tristanlegacque@orange.fr>
2018-03-22 11:34:36 +0100

    Liaison aux webservices

--- src/eseo/hotel/servlets/annulerReservation.java
+++ src/eseo/hotel/servlets/annulerReservation.java
@@ -1,5 +1,7 @@
 package eseo.hotel.servlets;
 
+import eseo.hotel.clientws.GestionHotelsSEI;
+import eseo.hotel.clientws.GestionHotelsService;
 import eseo.hotel.utils.TemplateUtil;
 
 import javax.servlet.ServletException;
@@ -8,52 +10,62 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.ParseException;
 
 /**
  * Servlet implementation class reservationServlet
  */
 @WebServlet("/annulerReservation")
 public class annulerReservation extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public annulerReservation() {
-		super();
-	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public annulerReservation() {
+        super();
+    }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Redirection vers la Servlet de gestion des reservation
-		request.getRequestDispatcher("/gererReservation").forward(request, response);
-	}
-
-
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Requête d'annulation de réservation
-		String idReservation 	= request.getParameter("idReservation");
+    /**
+     * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Redirection vers la Servlet de gestion des reservation
+        request.getRequestDispatcher("/gererReservation").forward(request, response);
+    }
 
 
-		//TODO : APPEL DU WEBSERVICE - Annulation reservation
-		boolean result = false;
-		//estionHotelsSEI service = new GestionHotelsService().getGestionHotelsPort();
-		//boolean resultat = true;//service.annulerChambre(Integer.parseInt(idReservation));
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Requête d'annulation de réservation
+        String idReservationStr = request.getParameter("idReservation");
+        Integer idReservation   = null;
+        String callbackType 	= null;
+        String callbackMessage 	= null;
 
-		String callbackType 	= null;
-		String callbackMessage 	= null;
-		if(result) {
-			callbackType 	= "success";
-			callbackMessage = "La réservation n°" + idReservation + " a bien été annulée !";
-		} else {
-			callbackType 	= "danger";
-			callbackMessage = "La réservation n°" + idReservation + " n'existe pas !";
-		}
+        //Conversion de l'ID en integer
+        try {
+            idReservation = Integer.parseInt(idReservationStr);
+        } catch(NumberFormatException ignored) {}
 
-		TemplateUtil.setCallback(request, response, callbackType, callbackMessage);
-		doGet(request,response);
-	}
+        //Si les paramètres sont valides
+        if(idReservation != null) {
+            //APPEL DU WEBSERVICE - Annulation reservation
+            GestionHotelsSEI service    = new GestionHotelsService().getGestionHotelsPort();
+
+            if(service.annulerChambre(idReservation)) {
+                callbackType 	= "success";
+                callbackMessage = "La réservation n°" + idReservation + " a bien été annulée !";
+            } else {
+                callbackType 	= "danger";
+                callbackMessage = "La réservation n°" + idReservation + " n'existe pas !";
+            }
+        } else {
+            callbackType 	= "danger";
+            callbackMessage = "Le numéro de réservation est invalide !";
+        }
+
+        TemplateUtil.setCallback(request, response, callbackType, callbackMessage);
+        doGet(request,response);
+    }
 }
